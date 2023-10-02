bd38dc58295ff732020a021e92232d31038e81bc
Karol Kropisz <karolkropisz@gmail.com>
2023-10-02 16:59:04 +0200

    Add JS code

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+
+let playerMove = 'papier';
+printMessage(' Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz! ');
