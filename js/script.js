3d8fe5503cf22a3603ee8ada9974c03c458468a1
Karol Kropisz <karolkropisz@gmail.com>
2023-10-02 18:51:42 +0200

    game almost works

--- js/script.js
+++ js/script.js
@@ -8,5 +8,75 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-let playerMove = 'papier';
-printMessage(' Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz! ');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+
+else if(randomNumber == 2){
+	computerMove = 'papier';
+}
+
+else if(randomNumber == 3){
+	computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+else if(playerInput == '2'){
+  playerMove = 'papier';
+}
+
+else if(playerInput == '3'){
+	playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+	printMessage('Ty wygrywasz!');
+}
+
+else if( computerMove == 'papier' && playerMove == 'nożyce'){
+	printMessage('Ty wygrywasz!');
+}
+
+else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+	printMessage('Ty wygrywasz!');
+}
+
+else if( computerMove == 'papier' && playerMove == 'kamień'){
+	printMessage('Ty przegrywasz!');
+}
+
+else if( computerMove == 'nożyce' && playerMove == 'papier'){
+	printMessage('Ty przegrywasz!');
+}
+
+else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+	printMessage('Ty przegrywasz!');
+}
+
+else if( computerMove == playerMove){
+	printMessage('remis');
+}
+
+else if ( playerMove == 4,5,6,7,8,9){
+	printMessage('błąd, wybierz numer od 1 do 3');
+}
