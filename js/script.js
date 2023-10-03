13807902865921d8cb9c7e5904c8a721213230f7
Karol Kropisz <karolkropisz@gmail.com>
2023-10-03 22:11:26 +0200

    Project Completion

--- js/script.js
+++ js/script.js
@@ -8,12 +8,48 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+	let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+	console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+function getMoveName(argMoveId){
+	if(argMoveId == 1){
+	return 'kamień';
+	}
+	if(argMoveId == 2){
+	return 'papier';
+	}
+	if(argMoveId == 3){
+	return 'nożyce';
+	}
+	
+	printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+	return 'nieznany ruch';
+}
 
+function displayResult(argComputerMove, argPlayerMove){
+	console.log('moves:', argComputerMove, argPlayerMove);
+	console.log('Wywyołanie Funkcji');
+
+	printMessage('Wybrałem: ' + argPlayerMove + ' , ' + ' PC wybrało:' + argComputerMove);
+
+	if( argComputerMove == argPlayerMove){
+		printMessage('Remis');
+	}
+	else if (
+		(argComputerMove == 'kamień' && argPlayerMove =='papier') ||
+		(argComputerMove == 'papier' && argPlayerMove =='nożyce') ||
+		(argComputerMove == 'nożyce' && argPlayerMove =='kamień')
+	){
+		printMessage('Wygrałeś');
+	}	
+	else{
+		printMessage('Przegrałeś');
+	}
+}
+
+	let argComputerMove = getMoveName(randomNumber);
+/*
 if(randomNumber == 1){
   computerMove = 'kamień';
 }
@@ -25,15 +61,17 @@ else if(randomNumber == 2){
 else if(randomNumber == 3){
 	computerMove = 'nożyce';
 }
+*/
 
-printMessage('Mój ruch to: ' + computerMove);
+	let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+	console.log('Gracz wpisał: ' + playerInput);
 
-console.log('Gracz wpisał: ' + playerInput);
+	let argPlayerMove = getMoveName(playerInput);
 
-let playerMove = 'nieznany ruch';
+	displayResult(argComputerMove, argPlayerMove);
 
+/*
 if(playerInput == '1'){
   playerMove = 'kamień';
 }
@@ -80,3 +118,4 @@ else if( computerMove == playerMove){
 else if ( playerMove == 4,5,6,7,8,9){
 	printMessage('błąd, wybierz numer od 1 do 3');
 }
+*/
